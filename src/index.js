6604f7e6b1408191b4acae988699b4e08f8799ea
fairyname-rgb <fairyname17@gmail.com>
2020-10-24 19:02:54 +0300

    towel-sort: final-commit

--- src/index.js
+++ src/index.js
@@ -2,5 +2,17 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let answer = [];
+  if(matrix === undefined){
+  return answer;
+}
+for (let i = 0; i < matrix.length; i++){
+  if (i % 2 !== 0){
+    matrix[i].reverse();
+    }
+  };
+  for (let m = 0; m < matrix.length; m++) {
+    answer = answer.concat(matrix[m]);   
+  };
+  return answer;
 }
